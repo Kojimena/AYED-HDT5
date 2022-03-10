fa8d358eeaf8adaebe1158a47cfcc7bce0f15d25
Jimena Hernández <77686378+Kojimena@users.noreply.github.com>
2022-03-10 11:13:35 -0600

    properties

--- HDT5.py
+++ HDT5.py
@@ -2,3 +2,20 @@
 #Algoritmos y Estructura de Datos
 #Creadora: Jimena Hernández carné: 21199
 #Viernes 11 de Marzo de 2022
+
+#Módulos que deben ser importados
+import simpy
+import random
+import statistics 
+
+RANDOM_SEED = 20 #Semilla
+
+RAM = 100 #Ram total
+
+CPU = 1 #Un proceso por unidad de tiempo
+
+PROCESOS =25 #Cantidad de procesos iniciales
+
+INTERVALO=10 #Intervalo de catidad de procesos totales
+
+INSTRUCCIONES = 3 #Instrucciones por minuto
